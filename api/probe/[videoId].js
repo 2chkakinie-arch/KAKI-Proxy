83dd513f1155347de236fc72000b568629e11705
2chkakinie-arch <2ch.kakinie@gmail.com>
2026-06-18 23:00:08 +0900

    Delete api directory

--- api/probe/[videoId].js
+++
@@ -1,7 +0,0 @@
-const server = require('../../index.js');
-
-module.exports = (req, res) => {
-  const { videoId } = req.query || {};
-  if (videoId) req.url = `/probe/${videoId}${req.url.includes('?') ? '?' + req.url.split('?')[1] : ''}`;
-  server.emit('request', req, res);
-};
