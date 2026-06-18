4f624a424e6e74f5a6cd0b90934ebfd55a08966f
2chkakinie-arch <2ch.kakinie@gmail.com>
2026-06-18 22:43:05 +0900

    Delete index.js

--- index.js
+++
@@ -1,7 +0,0 @@
-const server = require('../index.js');
-
-module.exports = (req, res) => {
-  // Default route — show health info
-  if (!req.url || req.url === '/api' || req.url === '/api/') req.url = '/';
-  server.emit('request', req, res);
-};
